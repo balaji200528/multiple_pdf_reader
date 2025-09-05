b5de77ce340596f662314b290bea538ef1442930
balaji28305 <balaji28305@gmail.com>
2025-09-05 19:04:33 +0530

    Add .gitignore to exclude myenv and Python artifacts

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from pypdf import PdfReader
+from langchain.text.splitter import CharacterTextSplitter
+import os
+
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+from langchain.chains.question_answering import load_qa_chain
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
