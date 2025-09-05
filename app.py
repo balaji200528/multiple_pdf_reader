ceddce2c4983f5c94f8732f2513cc163cdf40560
balaji28305 <balaji28305@gmail.com>
2025-09-05 19:56:44 +0530

    reading the pdf page by page

--- app.py
+++ app.py
@@ -13,4 +13,14 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader=PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+=page.extract_text()
+    return text
+
 
